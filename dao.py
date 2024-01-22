90e436d61f1b2d209bf29b599722e23c8e0d2929
Joel von der Weid <j.vonderweid@cche.ch>
2024-01-22 13:51:40 +0100

    Use mongo

--- dao.py
+++ dao.py
@@ -1,10 +1,15 @@
 from __future__ import annotations
-from typing import List, Union
+from typing import List
 from datetime import datetime
-import copy
 
-from pydantic import BaseModel
+from pymongo import MongoClient, DESCENDING
+from bson.objectid import ObjectId
+from pydantic import BaseModel, ValidationError
 
+def connect_db(): 
+   CONNECTION_STRING = "mongodb://localhost:27017"
+   client = MongoClient(CONNECTION_STRING)
+   return client['clip']['notes']
 
 class ClipFile(BaseModel):
     name: str
@@ -17,47 +22,41 @@ class ClipNoteDTO(BaseModel):
     files: List[ClipFile]
 
 class ClipNote(BaseModel):
-    id: int
+    id: str | None
     creation_date: datetime
     text: str | None
     files: List[ClipFile]
 
-fake_list: List[ClipNote] = []
-next_id = 1
+    def from_db_obj(db_obj: object) -> ClipNote:
+        return ClipNote(
+            id=str(db_obj._id),
+            creation_date=db_obj.creation_date,
+            text=db_obj.text,
+            files=[ClipFile(**f) for f in db_obj.files]
+        )
+
+db_collec = connect_db()
 
 def get_notes() -> List[ClipNote]:
-    # must return note orber by date desc
-    return fake_list
+    return [ClipNote.from_db_obj(n) for n in db_collec.find().sort('creation_date', DESCENDING)]
 
-def get_note(id: int) -> ClipNote | None:
-    for e in fake_list:
-        if e.id == id:
-            return e
+def get_note(id: str) -> ClipNote | None:
+    n = db_collec.find_one({"_id": ObjectId(id)})
+    return ClipNote.from_db_obj(n)
 
 def add_note(note_dto: ClipNoteDTO) -> ClipNote:
-    global next_id
-    global fake_list
-    
     note = ClipNote(
-        id = next_id,
+        id = None,
         creation_date = datetime.now(),
         text = note_dto.text,
         files = note_dto.files
     )
-    next_id += 1
-    fake_list.append(note)
+    res = db_collec.insert_one(note.model_dump(exclude=['id']))
+    note.id = str(res.inserted_id)
     return note
 
 # Delete a note
 # Return the note if found, None otherwise
 def delete_note(id: int) -> ClipNote | None:
-    el = None
-    for e in fake_list:
-        if e.id == id:
-            el = e
-            break
-    
-    if el != None:
-        fake_list.remove(el)
-    
-    return el
+    res = db_collec.find_one_and_delete({"_id": ObjectId(id)})
+    return ClipNote.from_db_obj(res)
