7939dc8c75cfff39249aacf9b6433137a9cf2193
joel.vonderwe <joel.vonderweid@master.hes-so.ch>
2024-01-27 18:34:38 +0100

    Add urlx_for

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Annotated, Optional, List
 from fastapi import FastAPI, Request, Response, status, UploadFile, File, Form
 from fastapi.templating import Jinja2Templates
+from jinja2 import pass_context
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -32,7 +33,18 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# Improve url_for to support HTTPS behind reverse proxy
+@pass_context
+def urlx_for(context: dict, name: str, **path_params) -> str:
+    request: Request = context['request']
+    http_url = request.url_for(name, **path_params)
+    if scheme := request.headers.get('x-forwarded-proto'):
+        return http_url.replace(scheme=scheme)
+    return http_url
+
 templates = Jinja2Templates("templates")
+templates.env.globals['url_for'] = urlx_for
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
